cf8f01cc163a3e70dd0c17e3ff4e18fff3cbc161
LionelAuroux <lionel.auroux@gmail.com>
2018-09-03 01:00:48 +0200

    WIP

--- litedb/litedb.py
+++ litedb/litedb.py
@@ -1,5 +1,6 @@
 import sqlite3
 from contextlib import contextmanager
+from recordclass import recordclass as rc
 
 class Session:
 
@@ -8,35 +9,54 @@ class Session:
 
     def __enter__(self):
         self.con = sqlite3.connect(self.db)
+        self.con.isolation_level = None
+        #self.con.set_trace_callback(print)
+        self.cursor = None
         return self
 
     def __exit__(self, *exc):
+        if self.cursor:
+            self.cursor.close()
         self.con.close()
         return None
 
+    def begin(self):
+        self.cursor = self.con.cursor()
+
+    def end(self):
+        self.cursor.close()
+
     def script(self, content):
-        self.con.executescript(content)
+        self.cursor.executescript(content)
     
     def sql(self, sql, param):
-        if isinstance(param, Table):
-            self.con.execute(sql, param.entry)
-        else:
-            self.con.execute(sql, param)
+        if isinstance(param, Table) or 'insert ' not in sql:
+            self.cursor.execute(sql, vars(param.entry))
+        elif isinstance(param, list):
+            pl = param
+            if isinstance(param[0], Table):
+                pl = [p.entry for p in param]
+            self.cursor.executemany(sql, pl)
 
     def init_query(self, sql, param):
-        self.cursor = self.con.cursor()
         self.cursor.execute(sql, param)
 
-    def fetch_res(self):
-        yield self.cursor.fetchone()
+    def fetch(self):
+        while True:
+            r = self.cursor.fetchone()
+            if not r:
+                return
+            yield r
 
     def fetch_all(self):
         return self.cursor.fetchall()
 
+    def fetch_one(self):
+        return self.cursor.fetchone()
+
     def commit(self):
         self.con.commit()
 
-from collections import namedtuple as nt
 
 class MetaTable(type):
     def __new__(cls, clsname, supercls, attrd):
@@ -46,7 +66,7 @@ class MetaTable(type):
             # search primary key
             pkey = None
             for k, v in attrd['fields'].items():
-                if ' primary key ' in v:
+                if ' primary key' in v:
                     if pkey:
                         raise TypeError("Table must have only one primary key")
                     pkey = k
@@ -73,11 +93,15 @@ class MetaTable(type):
             if fields:
                 attrd['insert'] = MetaTable.insert(clsname, sorted(fields.keys()))
                 attrd['update'] = MetaTable.update(clsname, sorted(fields.keys()), attrd['primary_key'])
-                attrd['namedtuple'] = nt(clsname, sorted(fields.keys()))
+                attrd['update_if'] = MetaTable.update_if(clsname)
+                attrd['delete'] = MetaTable.delete(clsname, attrd['primary_key'])
+                attrd['delete_if'] = MetaTable.delete_if(clsname)
+                attrd['recordclass'] = rc(clsname, sorted(fields.keys()))
+                print(repr(attrd['recordclass']))
         return type.__new__(cls, clsname, supercls, attrd)
 
     def insert(t, lsfield):
-        return "insert into {table} ({fields}) values ({ph});".format(table=t, fields=", ".join(lsfield), ph=",".join(['?'] * len(lsfield)))
+        return "insert into {table} ({fields}) values ({ph});".format(table=t, fields=", ".join(lsfield), ph=", ".join([':%s' % v for v in lsfield]))
 
     def update(t, lsfield, pkey):
         # extract primary key
@@ -86,13 +110,38 @@ class MetaTable(type):
             if n != pkey:
                 f.append(n)
         return "update {table} set {fields} where {condition};".format(
-                table=t, fields=", ".join([v + " = ?" for v in f]), condition="%s = ?" % pkey
+                table=t, fields=", ".join(["%s = :%s" % (v, v) for v in f]), condition="%s = :%s" % (pkey, pkey)
             )
 
+    def update_if(t):
+        return "update " + t + " set {fields} where {condition};"
+
+    def delete(t, pkey):
+        return "delete from {table} where {condition};".format(table=t, condition="%s = :%s" % (pkey, pkey))
+
+    def delete_if(t):
+        return "delete from " + t + " where {condition};"
+
 class Table(metaclass=MetaTable):
-    def __init__(self, *arg):
+    def __init__(self, *arg, **kwarg):
         t = type(self)
-        self.entry = t.namedtuple(*arg)
+        if len(arg):
+            self.entry = t.recordclass(*arg)
+        elif len(kwarg):
+            # add default (None) field
+            if len(kwarg) != len(t.fields):
+                for k in t.fields.keys():
+                    if k not in kwarg:
+                        kwarg[k] = None
+            self.entry = t.recordclass(**kwarg)
 
     def __repr__(self):
         return repr(self.entry)
+    
+    @classmethod
+    def do_insert(cls, s, param):
+        s.sql(cls.insert, param)
+
+    @classmethod
+    def do_update(cls, s, param):
+        s.sql(cls.update, param)
--- tests/test.py
+++ tests/test.py
@@ -4,20 +4,88 @@ import litedb as db
 
 class Entity(db.Table):
     """
-    Individual, or group, or kind/population
+    For test
     """
     fields = {
             'ent_id': "integer primary key autoincrement",
             'ent_type': "integer not null"
         }
 
+class Desc(db.Table):
+    fields = {
+        'desc_id': 'integer primary key autoincrement',
+        'desc_name': 'varchar(20) not null',
+        'desc_long': 'text'
+        }
+
 class LiteDB_Test(unittest.TestCase):
     def test_00(self):
+        """
+        Formatting test
+        """
+        # create
         self.assertTrue(hasattr(Entity, 'create'))
-        print(Entity.create)
-        self.assertTrue(hasattr(Entity, 'insert'))
-        print(Entity.insert)
-        self.assertTrue(hasattr(Entity, 'update'))
-        print(Entity.update)
+        cr = ("create table Entity (\n" +
+            "ent_id integer primary key autoincrement,\n" +
+            "ent_type integer not null\n)")
+        self.assertEqual(Entity.create, cr, "Entity.create not formatted correctly")
+        # reset
         self.assertTrue(hasattr(Entity, 'reset'))
-        print(Entity.reset)
+        rs = "drop table if exists Entity;\n" + cr
+        self.assertEqual(Entity.reset, rs, "Entity.reset not formatted correctly")
+        # insert
+        self.assertTrue(hasattr(Entity, 'insert'))
+        self.assertEqual(Entity.insert, "insert into Entity (ent_id, ent_type) values (:ent_id, :ent_type);", "Entity.insert not formatted correctly")
+        # update
+        self.assertTrue(hasattr(Entity, 'update'))
+        self.assertEqual(Entity.update, "update Entity set ent_type = :ent_type where ent_id = :ent_id;", "Entity.update not formatted correctly")
+        # delete
+        self.assertTrue(hasattr(Entity, 'delete'))
+        self.assertEqual(Entity.delete, "delete from Entity where ent_id = :ent_id;", "Entity.delete not formatted correctly")
+
+    def test_01(self):
+        import os
+        # reset if any
+        if os.path.exists("Model.db"):
+            os.remove("Model.db")
+        # just test a session
+        t1 = False
+        with db.Session("Model.db") as s:
+            if os.path.exists("Model.db"):
+                t1 = True
+                s.begin()
+                s.script(Desc.reset)
+                s.end()
+        self.assertTrue(t1)
+        # insert some data
+        with db.Session("Model.db") as s:
+            s.begin()
+            Desc.do_insert(s, [Desc(desc_name="test", desc_long="this is a test"),
+                    Desc(desc_name="test2", desc_long="this is a test2"),
+                    Desc(desc_name="test3", desc_long="this is a test3")])
+            s.end()
+            s.begin()
+            Desc.do_insert(s, Desc(desc_name="test4", desc_long="this is a test4"))
+            s.end()
+        # fetch
+        with db.Session("Model.db") as s:
+            s.begin()
+            s.init_query("select desc_id from Desc where desc_long like ?", ["this%"])
+            self.assertEqual(len(list(s.fetch())), 4, "count of inserted rows are wrong")
+            s.end()
+        # update
+        with db.Session("Model.db") as s:
+            s.begin()
+            s.init_query("select * from Desc where desc_name=?", ["test2"])
+            row = Desc(*s.fetch_one())
+            s.end()
+            theid = row.entry.desc_id
+            row.entry.desc_name = 'test_new'
+            s.begin()
+            Desc.do_update(s, row)
+            s.end()
+            s.begin()
+            s.init_query("select * from Desc where desc_id=?", [theid])
+            row = Desc(*s.fetch_one())
+            s.end()
+            self.assertEqual(row.entry.desc_name, 'test_new', "update failed")
